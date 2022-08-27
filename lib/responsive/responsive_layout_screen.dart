1c5e703957d91ffd0d453048032e0c6c182dd04a
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-08-26 21:20:22 -0300

    projeto criado

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/dimensions.dart';
+
+class ResponsiveLayoutScreen extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayoutScreen(
+      {Key? key,
+      required this.webScreenLayout,
+      required this.mobileScreenLayout})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxHeight > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}

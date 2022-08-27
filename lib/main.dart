ed9579f2b1ace8a658fd3245d00d8a93fe6ac0a4
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-08-26 22:44:19 -0300

    feita configuração do firebase pra android e web

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  // INICIAR USO DO FIREBASE
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    // PRA RODAR NA WEB
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAggGeswNELanWog5q8ApaBU4_1ddbMfaM',
+        appId: '1:40971141199:web:47618cf151f42cbc20ea3a',
+        messagingSenderId: '40971141199',
+        projectId: 'instagram-clone-flutter-70379',
+        storageBucket: 'instagram-clone-flutter-70379.appspot.com',
+      ),
+    );
+  } else {
+    // PRA RODAR NO ANDROID
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

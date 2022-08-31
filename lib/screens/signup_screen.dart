3bd29a83e06544f761de9dc5fd489327b8a0847f
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-08-30 22:51:52 -0300

    criado ui da tela de criar conta

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,22 +3,26 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/widgets/text_field_input.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _bioController = TextEditingController();
+  final _usernameController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _emailController.dispose();
     _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -45,13 +49,47 @@ class _LoginScreenState extends State<LoginScreen> {
               ],
             ),
             const SizedBox(height: 64),
+            // CIRCULAR PRA PEGAR FT DE PERFIL
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      'https://99prod.s3.amazonaws.com/uploads/f1350adc-bdfd-43bf-b210-78d1919cbcf5/Google.png'),
+                ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add_a_photo),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+
+            // USERNAME
+            TextFieldInput(
+              controller: _usernameController,
+              hintText: 'Digite um nome de usuário',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 24),
+            // BIO
+            TextFieldInput(
+              controller: _bioController,
+              hintText: 'Digite uma biografia',
+              textInputType: TextInputType.text,
+            ),
+            const SizedBox(height: 24),
             // EMAIL
             TextFieldInput(
               controller: _emailController,
               hintText: 'Digite o email',
               textInputType: TextInputType.emailAddress,
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 24),
             // SENHA
             TextFieldInput(
               controller: _passwordController,
@@ -87,7 +125,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 Container(
                   padding: const EdgeInsets.symmetric(vertical: 8),
                   child: const Text(
-                    'ñ tem conta? fodase ',
+                    'ñ tem conta? fodase  ',
                   ),
                 ),
                 GestureDetector(
